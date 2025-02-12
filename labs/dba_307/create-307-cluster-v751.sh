16770c02ea82a2b7a4242b6289484f2be0decbbc
Guanglei BAO <toddbgl@hotmail.com>
2025-02-13 06:43:28 +0800

    minor update

--- labs/dba_307/create-307-cluster-v751.sh
+++ labs/dba_307/create-307-cluster-v751.sh
@@ -3,4 +3,4 @@ REGION_NAME=${1}
 
 source ./cloud-env.sh
 
-./create-307-cluster-vinput.sh 7.5.1 ${REGION_NAME}
+./create-cluster-vinput.sh 7.5.1 ${REGION_NAME}
--- labs/dba_307/create-307-cluster-v754.sh
+++ labs/dba_307/create-307-cluster-v754.sh
@@ -3,4 +3,4 @@ REGION_NAME=${1}
 
 source ./cloud-env.sh
 
-./create-307-cluster-vinput.sh 7.5.4 ${REGION_NAME}
+./create-cluster-vinput.sh 7.5.4 ${REGION_NAME}
