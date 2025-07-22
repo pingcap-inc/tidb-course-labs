ce509fbf12a3a6fc475df00d9a408aa96e3b6584
wd0517 <me@wangdi.ink>
2025-07-22 10:31:03 +0800

    dba_307: fix create-cluster-vinput script

--- labs/dba_307/create-cluster-vinput.sh
+++ labs/dba_307/create-cluster-vinput.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 VERSION=${1}
 REGION_NAME=${2}
+TOPOLOGY_FILE=${3:-./solution-topology-eight-nodes.yaml}
 
 source ./cloud-env.sh
 
@@ -8,7 +9,7 @@ source ./cloud-env.sh
 ./01-precheck-307-and-fix-nodes.sh ${REGION_NAME}
 
 # Creating the TiDB cluster named tidb-test, version ${VERSION}
-~/.tiup/bin/tiup cluster deploy tidb-test ${VERSION} ./solution-topology-eight-nodes.yaml --user ec2-user -i /home/ec2-user/.ssh/pe-class-key-${REGION_NAME}.pem --yes
+~/.tiup/bin/tiup cluster deploy tidb-test ${VERSION} ${TOPOLOGY_FILE} --user ec2-user -i /home/ec2-user/.ssh/pe-class-key-${REGION_NAME}.pem --yes
 sleep 3;
 
 ~/.tiup/bin/tiup cluster start tidb-test
