6da0859d8eabec0af59aa6e131fbae684a41cac7
Guanglei BAO <toddbgl@hotmail.com>
2025-07-22 11:02:34 +0800

    lab(dba_307): scripts fix

--- labs/dba_307/create-cluster-vinput.sh
+++ labs/dba_307/create-cluster-vinput.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 VERSION=${1}
 REGION_NAME=${2}
 TOPOLOGY_FILE=${3:-./solution-topology-eight-nodes.yaml}
@@ -6,7 +8,7 @@ TOPOLOGY_FILE=${3:-./solution-topology-eight-nodes.yaml}
 source ./cloud-env.sh
 
 ~/.tiup/bin/tiup mirror set tidb-community-server-${VERSION}-linux-amd64
-./01-precheck-307-and-fix-nodes.sh ${REGION_NAME}
+./01-precheck-307-and-fix-nodes.sh ${TOPOLOGY_FILE} ${REGION_NAME}
 
 # Creating the TiDB cluster named tidb-test, version ${VERSION}
 ~/.tiup/bin/tiup cluster deploy tidb-test ${VERSION} ${TOPOLOGY_FILE} --user ec2-user -i /home/ec2-user/.ssh/pe-class-key-${REGION_NAME}.pem --yes >> /home/ec2-user/create-cluster-vinput.log 2>&1
