7f3c874b9d5efcf1ddb108a6038015d935cf52eb
Guanglei BAO <toddbgl@hotmail.com>
2025-07-24 21:53:17 +0800

    dynamic dba_307 create-cluster-vinput.sh

--- labs/dba_303/01-precheck-307-and-fix-nodes.sh
+++ labs/dba_303/01-precheck-307-and-fix-nodes.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-REGION_NAME=${1}
+#REGION_NAME=${1}
 
 # curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh
 
-~/.tiup/bin/tiup cluster check solution-topology-eight-nodes.yaml --user ec2-user -i /home/ec2-user/.ssh/pe-class-key-${REGION_NAME}.pem --apply
+#~/.tiup/bin/tiup cluster check solution-topology-eight-nodes.yaml --user ec2-user -i /home/ec2-user/.ssh/pe-class-key-${REGION_NAME}.pem --apply
+
+#echo "############################################################################################"
+#echo "## It's safe to ignore disk and cpu-governor check failures in this classroom environment. #"
+#echo "############################################################################################"
+
+set -e
+
+TOPOLOGY_FILE=${1}
+REGION_NAME=${2}
+
+# curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh
+
+~/.tiup/bin/tiup cluster check ${TOPOLOGY_FILE} --user ec2-user -i /home/ec2-user/.ssh/pe-class-key-${REGION_NAME}.pem --apply
 
 echo "############################################################################################"
 echo "## It's safe to ignore disk and cpu-governor check failures in this classroom environment. #"
--- labs/dba_303/create-cluster-vinput.sh
+++ labs/dba_303/create-cluster-vinput.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
+#VERSION=${1}
+#REGION_NAME=${2}
+
+#source ./cloud-env.sh
+
+#~/.tiup/bin/tiup mirror set tidb-community-server-${VERSION}-linux-amd64
+#./01-precheck-and-fix-nodes.sh ${REGION_NAME}
+
+# Creating the TiDB cluster named tidb-test, version ${VERSION}
+#~/.tiup/bin/tiup cluster deploy tidb-test ${VERSION} ./solution-topology-ten-nodes.yaml --user ec2-user -i /home/ec2-user/.ssh/pe-class-key-${REGION_NAME}.pem --yes
+#sleep 3;
+
+#~/.tiup/bin/tiup cluster start tidb-test
+
+set -e
+
 VERSION=${1}
 REGION_NAME=${2}
+TOPOLOGY_FILE=${3:-./solution-topology-ten-nodes.yaml}
 
 source ./cloud-env.sh
 
 ~/.tiup/bin/tiup mirror set tidb-community-server-${VERSION}-linux-amd64
-./01-precheck-and-fix-nodes.sh ${REGION_NAME}
+./01-precheck-307-and-fix-nodes.sh ${TOPOLOGY_FILE} ${REGION_NAME}
 
 # Creating the TiDB cluster named tidb-test, version ${VERSION}
-~/.tiup/bin/tiup cluster deploy tidb-test ${VERSION} ./solution-topology-ten-nodes.yaml --user ec2-user -i /home/ec2-user/.ssh/pe-class-key-${REGION_NAME}.pem --yes
+~/.tiup/bin/tiup cluster deploy tidb-test ${VERSION} ${TOPOLOGY_FILE} --user ec2-user -i /home/ec2-user/.ssh/pe-class-key-${REGION_NAME}.pem --yes >> /home/ec2-user/create-cluster-vinput.log 2>&1
 sleep 3;
 
 ~/.tiup/bin/tiup cluster start tidb-test
