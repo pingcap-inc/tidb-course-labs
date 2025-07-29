9b61f6cfa3c37d9dee4bd9647184a618c2a1aa6b
Chenhao Huang <chenhao.tony.huang@gmail.com>
2025-07-29 16:52:32 +1000

    demo_009 code update:

---
+++ labs/demo_009/demo_connection_tiproxy_no_reconnect.py
@@ -0,0 +1,45 @@
+from mysql.connector import connect
+from mysql.connector import Error
+from _mysql_connector import MySQLInterfaceError
+import random
+import time
+
+if __name__ == "__main__":
+
+    while True:
+        try:
+            conn = connect(
+                database="test",
+                host="10.90.4.253",
+                port=6000,
+                user="root",
+                password="",
+            )
+            print("Connected to TiDB:")
+            cursor = conn.cursor(prepared=True)
+
+            while True:
+                random_integer = random.randint(0, 10)
+                query = "UPDATE test.users SET balance = " + str(random_integer) + " WHERE id = 3;"
+
+                time.sleep(0.5)
+                try:
+                    cursor.execute(query)
+                    conn.commit()
+                    print("Committed. Charlie's balance updated to: ", random_integer)
+
+                except Error as e:
+                    print("Error during query execution:", e)
+                    cursor.close()
+                    conn.close()
+                    break
+
+        except (Error, MySQLInterfaceError) as connect_err:
+            print("Connection error:", connect_err)
+            # Only close if cursor/conn exist
+            try:
+                cursor.close()
+                conn.close()
+            except:
+                pass
+            break
