cf5e02e5e862664a9d70afb6c14616e8f380b99c
Guanglei BAO <toddbgl@hotmail.com>
2025-11-18 14:09:43 +0800

    lab(demo_203) update

---
+++ labs/demo_203/books_populate.sql
@@ -0,0 +1,707 @@
+INSERT INTO `books` (
+        `title`,
+        `type`,
+        `published_at`,
+        `stock`,
+        `price`
+    )
+VALUES (
+        'Greatest Love of All',
+        'Education & Reference',
+        '2017-02-18 15:38:29',
+        16,
+        99.00
+    ),
+    (
+        'The Power of Love',
+        'Novel',
+        '2011-07-14 09:28:40',
+        2147483645,
+        69.02
+    ),
+    (
+        'Unchained Melody',
+        'Science & Technology',
+        '2016-05-09 14:53:08',
+        66,
+        13.00
+    ),
+    (
+        'Higher Love',
+        'Humanities & Social Sciences',
+        '2010-06-02 12:08:16',
+        12,
+        1.00
+    ),
+    (
+        'Waterfalls',
+        'Sports',
+        '2022-05-07 09:49:37',
+        1,
+        159.51
+    ),
+    (
+        'Say You',
+        'Life',
+        '2001-03-21 22:50:17',
+        2000,
+        165.51
+    ),
+    (
+        'Drop it Like It\'s Hot',
+        'Sports',
+        '2013-03-18 22:29:34',
+        32,
+        59.68
+    ),
+    (
+        'Dilemma',
+        'Science & Technology',
+        '2013-02-19 10:52:24',
+        990,
+        197.15
+    ),
+    (
+        'Wishing Well',
+        'Magazine',
+        '2012-10-26 16:01:49',
+        212121,
+        80.87
+    ),
+    (
+        'Venus',
+        'Magazine',
+        '2013-08-07 16:57:37',
+        1000221,
+        174.36
+    ),
+    (
+        'Use Somebody',
+        'Life',
+        '2008-12-01 05:05:35',
+        3524321,
+        116.49
+    ),
+    (
+        'Just My Imagination (Running Away With Me)',
+        'Science & Technology',
+        '2021-10-20 04:17:02',
+        30000,
+        56.56
+    ),
+    (
+        'Gonna Make You Sweat (Everybody Dance Now)',
+        'Kids',
+        '2006-10-19 17:43:32',
+        456456,
+        45.94
+    ),
+    (
+        'Thrift Shop',
+        'Novel',
+        '2005-02-06 23:00:56',
+        1125,
+        180.19
+    ),
+    (
+        'Faith',
+        'Novel',
+        '2002-06-22 21:36:06',
+        6,
+        107.25
+    ),
+    (
+        'Whole Lotta Shakin\' Goin\' On',
+        'Magazine',
+        '2001-01-07 02:29:41',
+        1009,
+        77.77
+    ),
+    (
+        'Addicted to Love',
+        'Education & Reference',
+        '2019-09-02 04:26:24',
+        4,
+        83.80
+    ),
+    (
+        'Goodbye Yellow Brick Road',
+        'Education & Reference',
+        '2019-07-06 04:41:57',
+        168,
+        193.83
+    ),
+    (
+        'Give Me Everything',
+        'Arts',
+        '2018-12-14 08:17:23',
+        111111,
+        73.89
+    ),
+    (
+        'All Along the Watchtower',
+        'Science & Technology',
+        '2021-05-23 19:52:42',
+        5555555,
+        104.12
+    ),
+    (
+        'Every Breath You Take',
+        'Life',
+        '2012-10-05 16:42:45',
+        4,
+        177.81
+    ),
+    (
+        'Da Doo Ron Ron (When He Walked Me Home)',
+        'Life',
+        '2012-10-12 18:52:38',
+        45,
+        87.61
+    ),
+    (
+        'Chattanoogie Shoe-Shine Boy',
+        'Life',
+        '2015-08-19 13:54:57',
+        1330000,
+        89.04
+    ),
+    (
+        'I\'m So Lonesome I Could Cry',
+        'Science & Technology',
+        '2016-07-11 08:56:10',
+        4981,
+        133.59
+    ),
+    (
+        'Daydream Believer',
+        'Science & Technology',
+        '2008-08-23 10:28:50',
+        2147483647,
+        101.02
+    ),
+    (
+        'Georgia On My Mind',
+        'Arts',
+        '2020-01-21 14:45:20',
+        2146,
+        92.42
+    ),
+    (
+        'Play That Funky Music',
+        'Science & Technology',
+        '2018-08-02 11:04:36',
+        147,
+        185.76
+    ),
+    (
+        'Fortunate Son',
+        'Humanities & Social Sciences',
+        '2005-04-24 17:07:58',
+        171,
+        12.51
+    ),
+    (
+        'Jump',
+        'Arts',
+        '2018-03-20 16:22:32',
+        155,
+        87.06
+    ),
+    (
+        'Sexyback',
+        'Life',
+        '2001-09-05 04:04:13',
+        0,
+        12.17
+    ),
+    (
+        'Can\'t Buy Me Love',
+        'Arts',
+        '2015-08-07 10:45:17',
+        864,
+        27.64
+    ),
+    (
+        'Love Hangover',
+        'Science & Technology',
+        '2002-06-05 23:18:15',
+        9888998,
+        106.51
+    ),
+    (
+        'Groovin\'',
+        'Humanities & Social Sciences',
+        '2000-03-14 22:01:22',
+        0,
+        41.97
+    ),
+    (
+        'Take On Me',
+        'Life',
+        '2011-02-13 08:29:41',
+        181,
+        23.42
+    ),
+    (
+        'Hips don\'t lie',
+        'Life',
+        '2016-11-11 23:32:50',
+        4,
+        38.94
+    ),
+    (
+        'Livin\' La Vida Loca',
+        'Life',
+        '2009-06-23 20:35:22',
+        972,
+        47.51
+    ),
+    (
+        'What Goes Around Comes Around',
+        'Kids',
+        '2016-09-22 04:33:00',
+        1989,
+        111.16
+    ),
+    (
+        '12th Street Rag',
+        'Humanities & Social Sciences',
+        '2020-11-17 01:32:27',
+        184,
+        64.27
+    ),
+    (
+        'The Hustle',
+        'Sports',
+        '2018-12-09 13:18:06',
+        10,
+        1.95
+    ),
+    (
+        '(You Keep Me) Hangin\' On',
+        'Humanities & Social Sciences',
+        '2005-02-12 05:29:04',
+        163,
+        32.72
+    ),
+    (
+        'Build Me Up Buttercup',
+        'Science & Technology',
+        '2020-03-30 18:36:47',
+        30,
+        195.32
+    ),
+    (
+        'He\'s a Rebel',
+        'Arts',
+        '2016-02-12 23:03:06',
+        2147483,
+        131.47
+    ),
+    (
+        '(I\'ve Got a Gal In) Kalamazoo',
+        'Sports',
+        '2006-05-22 05:19:49',
+        231321,
+        85.72
+    ),
+    (
+        'Mood Indigo',
+        'Humanities & Social Sciences',
+        '2003-03-18 05:31:47',
+        78888,
+        19.70
+    ),
+    (
+        'Killing Me Softly With His Song',
+        'Magazine',
+        '2014-07-03 01:42:14',
+        8,
+        184.11
+    ),
+    (
+        'Mrs Robinson',
+        'Comics',
+        '2020-12-18 06:24:25',
+        80000,
+        155.36
+    ),
+    (
+        'Spanish Harlem',
+        'Magazine',
+        '2002-07-09 11:27:39',
+        1799998,
+        98.66
+    ),
+    (
+        'White Christmas',
+        'Life',
+        '2000-05-22 02:33:53',
+        1,
+        64.54
+    ),
+    (
+        'Cry',
+        'Science & Technology',
+        '2012-12-11 15:31:47',
+        141,
+        13.20
+    ),
+    (
+        'Hey There',
+        'Kids',
+        '2013-04-03 09:39:13',
+        1050,
+        98.98
+    ),
+    (
+        'Body & Soul',
+        'Humanities & Social Sciences',
+        '2001-10-02 02:15:38',
+        0,
+        90.29
+    ),
+    (
+        'Green River',
+        'Life',
+        '2016-01-23 09:04:42',
+        31,
+        17.64
+    ),
+    (
+        'Vaya Con Dios (may God Be With You)',
+        'Arts',
+        '2007-12-03 22:16:55',
+        10,
+        81.07
+    ),
+    (
+        'Royals',
+        'Life',
+        '2023-03-14 20:47:14',
+        16,
+        68.95
+    ),
+    (
+        'Unbelievable',
+        'Arts',
+        '2001-05-11 00:24:41',
+        137,
+        10.20
+    ),
+    (
+        'Play That Funky Music',
+        'Life',
+        '2020-11-30 12:37:17',
+        1191545453,
+        146.41
+    ),
+    (
+        'The Way You Move',
+        'Magazine',
+        '2002-07-17 07:14:00',
+        720,
+        0.84
+    ),
+    (
+        'Papa Was a Rolling Stone',
+        'Life',
+        '2005-10-10 12:04:12',
+        100,
+        64.73
+    ),
+    (
+        'Maybellene',
+        'Novel',
+        '2018-08-17 23:55:13',
+        12,
+        156.69
+    ),
+    (
+        'ABC',
+        'Education & Reference',
+        '2014-04-29 13:25:37',
+        11825,
+        74.95
+    ),
+    (
+        'One Bad Apple',
+        'Novel',
+        '2011-10-03 01:33:46',
+        1747,
+        191.90
+    ),
+    (
+        'I Just Called to Say I Love You',
+        'Sports',
+        '2022-09-13 08:12:18',
+        8,
+        80.96
+    ),
+    (
+        'Don\'t Let the Sun Go Down On Me',
+        'Education & Reference',
+        '2016-11-05 22:44:55',
+        61,
+        155.21
+    ),
+    (
+        'Bad Moon Rising',
+        'Sports',
+        '2003-04-25 12:55:35',
+        126,
+        4.28
+    ),
+    (
+        'Summer in the City',
+        'Novel',
+        '2023-04-29 17:05:33',
+        10,
+        26.86
+    ),
+    (
+        'Baby Got Back',
+        'Humanities & Social Sciences',
+        '2018-02-03 11:19:04',
+        243,
+        187.49
+    ),
+    (
+        'School\'s Out',
+        'Science & Technology',
+        '2021-06-04 22:28:20',
+        6,
+        109.95
+    ),
+    (
+        'Stayin\' Alive',
+        'Magazine',
+        '2018-12-18 05:02:10',
+        299,
+        63.95
+    ),
+    (
+        'Tiger Rag',
+        'Novel',
+        '2017-06-10 02:18:13',
+        111,
+        157.05
+    ),
+    (
+        'Someday',
+        'Arts',
+        '2023-07-16 18:18:00',
+        138,
+        72.60
+    ),
+    (
+        'Besame Mucho',
+        'Arts',
+        '2010-02-11 08:46:41',
+        2,
+        99.54
+    ),
+    (
+        'Up Up & Away',
+        'Life',
+        '2003-01-05 07:48:26',
+        144,
+        170.99
+    ),
+    (
+        'Blue Tango',
+        'Science & Technology',
+        '2006-07-11 08:29:36',
+        138,
+        94.34
+    ),
+    (
+        'My Sweet Lord',
+        'Sports',
+        '2000-09-13 12:50:00',
+        567165,
+        179.36
+    ),
+    (
+        'Be My Baby',
+        'Comics',
+        '2008-02-24 12:26:05',
+        335,
+        149.85
+    ),
+    (
+        'You Can\'t Hurry Love',
+        'Science & Technology',
+        '2022-01-07 07:10:08',
+        6,
+        187.77
+    ),
+    (
+        'Moonlight Cocktail',
+        'Magazine',
+        '2009-01-05 02:20:54',
+        999,
+        169.77
+    ),
+    (
+        'Eve of Destruction',
+        'Magazine',
+        '2022-07-05 13:24:09',
+        74,
+        108.12
+    ),
+    (
+        'Instant Karma',
+        'Kids',
+        '2015-10-04 06:02:08',
+        49,
+        131.04
+    ),
+    (
+        'I Got You Babe',
+        'Comics',
+        '2015-03-23 14:38:26',
+        167777,
+        94.00
+    ),
+    (
+        'Leader of the Pack',
+        'Kids',
+        '2007-03-20 16:22:26',
+        1,
+        56.32
+    ),
+    (
+        'Earth Angel',
+        'Comics',
+        '2010-10-06 13:43:50',
+        117,
+        194.68
+    ),
+    (
+        'Moonlight Serenade',
+        'Sports',
+        '2022-04-06 03:13:43',
+        145,
+        14.87
+    ),
+    (
+        'These Boots Are Made For Walking',
+        'Arts',
+        '2005-12-28 19:11:57',
+        60,
+        179.88
+    ),
+    (
+        'Help Me',
+        'Humanities & Social Sciences',
+        '2014-10-08 14:48:52',
+        1612,
+        141.97
+    ),
+    (
+        'Hit the Road',
+        'Life',
+        '2020-12-01 12:51:28',
+        1722,
+        19.35
+    ),
+    (
+        'The Last Dance',
+        'Education & Reference',
+        '2004-01-07 06:34:50',
+        11,
+        39.27
+    ),
+    (
+        '(Let Me Be Your) Teddy Bear',
+        'Sports',
+        '2003-04-08 08:37:52',
+        140,
+        22.14
+    ),
+    (
+        'Use Somebody',
+        'Education & Reference',
+        '2003-12-10 16:25:09',
+        1831,
+        70.64
+    ),
+    (
+        'The Sign',
+        'Science & Technology',
+        '2010-03-07 06:51:16',
+        38,
+        138.97
+    ),
+    (
+        'Sugar Sugar',
+        'Education & Reference',
+        '2015-06-11 02:18:41',
+        1495677,
+        23.07
+    ),
+    (
+        'Wishing Well',
+        'Comics',
+        '2012-06-14 19:20:08',
+        777,
+        166.09
+    ),
+    (
+        'Frenesi',
+        'Comics',
+        '2014-02-26 19:49:48',
+        83,
+        171.43
+    ),
+    (
+        'Ol\' Man River',
+        'Sports',
+        '2015-06-08 02:20:42',
+        7,
+        79.55
+    ),
+    (
+        'I Believe I Can Fly',
+        'Science & Technology',
+        '2018-03-28 14:50:19',
+        288,
+        197.48
+    ),
+    (
+        'Let it Snow! Let it Snow! Let it Snow!',
+        'Life',
+        '2004-03-28 20:58:00',
+        169,
+        192.84
+    ),
+    (
+        'Open Arms',
+        'Life',
+        '2013-04-14 21:31:25',
+        40,
+        182.34
+    ),
+    (
+        'On the Atchison',
+        'Comics',
+        '2011-10-17 14:47:04',
+        852,
+        185.59
+    ),
+    (
+        'Cars',
+        'Science & Technology',
+        '2009-01-31 08:26:26',
+        20,
+        75.24
+    ),
+    (
+        'Till The End of Time',
+        'Kids',
+        '2000-01-22 10:27:08',
+        25,
+        109.00
+    );
