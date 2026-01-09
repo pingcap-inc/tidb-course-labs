2d18cc6f8483a1ef1135dc553a7c964c6baafdfa
Chenhao Huang <chenhao.tony.huang@gmail.com>
2026-01-09 16:03:49 +1100

    minor fxi

--- labs/demo_204/Mshop/config/session.php
+++ labs/demo_204/Mshop/config/session.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'driver' => env('SESSION_DRIVER', 'database'),
+    'driver' => env('SESSION_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
@@ -129,7 +129,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug((string) env('APP_NAME', 'laravel')).'-session'
+        Str::slug((string) (env('APP_NAME') ?? 'laravel')).'-session'
     ),
 
     /*
