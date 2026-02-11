571d69d634f5d343546bdae0bd98c2a94606c680
dongfei <88232896+utopiadf@users.noreply.github.com>
2026-02-11 16:33:50 +0800

    Fix paging issue in 204, 205
    Fix paging issue in 204, 205.

--- labs/demo_205/Mshop/resources/views/transactions/listTransaction.blade.php
+++ labs/demo_205/Mshop/resources/views/transactions/listTransaction.blade.php
@@ -58,7 +58,7 @@
 
             {{-- Page navigation button --}}
             <div class="mt-4">
-                {{ $TransactionPaginate->links() }}
+                {{ $TransactionPaginate->withPath(url('/transactions'))->links() }}
             </div>
         </div>
     </div>
