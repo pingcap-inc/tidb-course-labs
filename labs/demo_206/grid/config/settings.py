935a6338d086a2b5e1938858c6ccb4363bdc5db4
Guanglei BAO <toddbgl@hotmail.com>
2026-02-13 17:04:23 +0800

    demo_206 refine

--- labs/demo_206/grid/config/settings.py
+++ labs/demo_206/grid/config/settings.py
@@ -80,7 +80,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'grid' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
